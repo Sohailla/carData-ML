efde4cbaaf91f7768e9a49a9560b87a61af6f686
“Sohaila <sohailagamal1112@gmail.com>
2023-03-25 02:47:43 +0200

    linear fun

--- 20196026_20206018_first.py
+++ 20196026_20206018_first.py
@@ -2,7 +2,6 @@ import pandas as pandas
 import matplotlib.pyplot as plot
 import numpy as numpy
 from sklearn.model_selection import train_test_split
-from pandas.plotting import scatter_matrix
 
 # A. Load the “car_data.csv” dataset.
 url = "car_data.csv"
@@ -16,101 +15,90 @@ dataset = pandas.read_csv(url)
 # B. Use scatter plots between different features (7 at least) and the car price to select 5 of the numerical features
 # that are positively/negatively correlated to the car price (i.e., features that have a relationship with the
 # target). These 5 features are the features that will be used in linear regression.
-dataset.plot(kind='scatter', x='carlength', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='carwidth', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='carheight', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='curbweight', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='enginesize', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='boreratio', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='stroke', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='peakrpm', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='citympg', y='price')
-plot.show()
-dataset.plot(kind='scatter', x='highwaympg', y='price')
-plot.show()
+# dataset.plot(kind='scatter', x='carlength', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='carwidth', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='carheight', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='curbweight', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='enginesize', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='boreratio', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='stroke', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='peakrpm', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='citympg', y='price')
+# plot.show()
+# dataset.plot(kind='scatter', x='highwaympg', y='price')
+# plot.show()
 
 # select 5 features that are correlated with price
-carInfo = ['carlength', 'carwidth', 'curbweight', 'enginesize', 'horsepower']
+features = ['carlength', 'carwidth', 'curbweight', 'enginesize', 'horsepower']
 
 # C. Split the dataset into training and testing sets.
-# data = dataset.sample(frac=0.8, random_state=42)  // pandas
-# test = dataset.drop(data.index)
-data, test = train_test_split(dataset, test_size=0.8, random_state=42)
+x = dataset[features]
+y = dataset["price"]
+# X_train: the training input features
+# X_test: the testing input features
+# y_train: the training target variable
+# y_test: the testing target variable
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # D. Implement linear regression from scratch using gradient descent to
 # optimize the parameters of the hypothesis function.
+
 # define the hypothesis function
-def hypothesis(Theta, X):
-    return numpy.dot(X, Theta)
+def hypothesis(X, theta):
+    return numpy.dot(X, theta)
 
 # define the cost function
-def cost(Theta, X, y):
-    m = len(y)
-    h = hypothesis(theta, X)
-    J = numpy.sum((h-y)**2)/(2*m)
-    return J
+def cost(X, Y, theta):
+    m = len(X)
+    h = hypothesis(X, theta)
+    M_squared_error = (h - Y) **2
+    calc_cost = 1/ (2*m) * numpy.sum(M_squared_error)
+    return  calc_cost
 
 # define the gradient descent function
-def gradient_descent(X, y, Theta, Alpha, num_iterations):
-    m = len(y)
-    costCycle = numpy.zeros(num_iterations)
-    for i in range(iterations):
-        h = hypothesis(Theta, X)
-        Theta = Theta - (Alpha / m) * numpy.dot(X.T, (h - y))
-        costCycle[i] = cost(theta, X, y)
-    return Theta, costCycle
-
-# prepare training and testing data
-train_X = numpy.array(data[carInfo])
-train_y = numpy.array(data['price'])
-test_X = numpy.array(test[carInfo])
-test_y = numpy.array(test['price'])
-
-# normalize training data
-mu = numpy.mean(train_X, axis=0)
-sigma = numpy.std(train_X, axis=0)
-train_X = (train_X - mu)/sigma
-
-# add bias term to training data
-train_X = numpy.insert(train_X, 0, 1, axis=1)
-
-# initialize theta and hyperparameters
-theta = numpy.zeros(train_X.shape[1])
-alpha = 0.01
-iterations = 1500
-
-# run gradient descent to optimize theta
-theta, cost_history = gradient_descent(train_X, train_y, theta, alpha, iterations)
-
-# print the optimized parameters
-print('Optimized parameters:')
-print(theta)
+# AND
 # F. Calculate the cost (mean squared error) in every iteration to see how the
 # error of the hypothesis function changes with every iteration of gradient
 # descent.
+def gradient_descent(X, Y, theta, alpha, n_iterations):
+    m = len(X)
+    result = []
+    for i in range(n_iterations):
+        h = hypothesis(X, theta)
+        calc_errors = h - Y
+        gradient = 1/ m * numpy.dot(X.T, calc_errors)
+        theta = theta - alpha * gradient
+        result.append(cost(X, Y, theta))
+    return theta, result
+
+# initialize theta and hypothesis parameters
+alpha = 0.01
+n_iterations = 1000
+theta = numpy.zeros(X_train.shape[1])
+
+# run gradient descent to optimize theta
+theta, result = gradient_descent(X_train, y_train, theta, alpha, n_iterations)
+
+# E. print the optimized parameters
+print('Optimized parameters:')
+print(theta)
 
 # G. Plot the cost against the number of iterations.
-plot.plot(cost_history)
+plot.plot(result)
 plot.xlabel('Iterations')
 plot.ylabel('Cost')
 plot.show()
 
-# normalize testing data
-test_X = (test_X - mu)/sigma
-
-# add bias term to testing data
-test_X = numpy.insert(test_X, 0, 1, axis=1)
-
 # H. Use the optimized hypothesis function to make predictions on the testing
 # set and calculate the accuracy of the final (trained) model on the test set.
-predictions = hypothesis(theta, test_X)
-accuracy = 1 - numpy.mean(numpy.abs(predictions - test_y)/test_y)
-print('Accuracy:', accuracy)
+h = hypothesis(theta, X_test)
+Accuracy = 1 - numpy.mean(numpy.abs(h - y_test)/y_test)
+print('Accuracy:', Accuracy)
--- 20196026_20206018_second.py
+++ 20196026_20206018_second.py
@@ -5,14 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# Load the dataset
+# A. Load the dataset
 dataset = pandas.read_csv("customer_data.csv")
 
-# Normalize the feature data
+# B. Normalize the feature data
 dataset["age"] = (dataset["age"] - dataset["age"].min()) / (dataset["age"].max() - dataset["age"].min())
 dataset["salary"] = (dataset["salary"] - dataset["salary"].min()) / (dataset["salary"].max() - dataset["salary"].min())
 
-# Scatter plot with different colors for the "purchased" feature
+# C. Scatter plot with different colors for the "purchased" feature
 plot.scatter(dataset[dataset["purchased"]==0]["age"], dataset[dataset["purchased"]==0]["salary"], color='r', label='Not purchased')
 plot.scatter(dataset[dataset["purchased"]==1]["age"], dataset[dataset["purchased"]==1]["salary"], color='b', label='Purchased')
 plot.xlabel("Age")
@@ -20,22 +20,22 @@ plot.ylabel("Salary")
 plot.legend()
 plot.show()
 
-# Split the dataset into training and testing sets
+# D. Split the dataset into training and testing sets
 X = dataset[["age", "salary"]]
 y = dataset["purchased"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Run logistic regression and optimize the parameters
+# E. Run logistic regression and optimize the parameters
 clf = LogisticRegression()
 clf.fit(X_train, y_train)
 
-# Print the parameters of the hypothesis function
+# F. Print the parameters of the hypothesis function
 print("Intercept:", clf.intercept_)
 print("Coefficients:", clf.coef_)
 
-# Use the optimized hypothesis function to make predictions on the testing set
-y_pred = clf.predict(X_test)
+# G. Use the optimized hypothesis function to make predictions on the testing set
+y_predictions = clf.predict(X_test)
 
 # Calculate the accuracy of the final (trained) model on the test set
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_predictions)
 print("Accuracy = ", accuracy)
